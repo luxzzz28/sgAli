d92101617e03e6afb8c729c08da18036ffef1626
luxzzz28 <teskanweb@gmail.com>
2024-10-03 02:00:52 +0700

    sg vultr

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '5ab86ee5-b9f7-440a-ade0-c0249581c6a3';
 
-const พร็อกซีไอพีs = ['51.79.158.126'];
+const พร็อกซีไอพีs = ['45.77.36.40'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
